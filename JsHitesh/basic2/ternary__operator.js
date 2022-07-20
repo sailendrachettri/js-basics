83141e22c7127734e0cae5e6ee004953880ea68c
sailendrachettri <sailendra9083@gmail.com>
2022-07-20 15:08:55 +0530

    ternary operator

---
+++ JsHitesh/basic2/ternary__operator.js
@@ -0,0 +1,18 @@
+let authencated = true;
+
+console.log();
+console.log();
+
+// if(authencated){
+//     console.log("show signout button");
+
+// }else{
+//     console.log("show signin options");
+// }
+
+// -----------------------------OR-----------------------------
+
+authencated ? console.log("show signout button") : console.log("show signin options");
+
+console.log();
+console.log();
