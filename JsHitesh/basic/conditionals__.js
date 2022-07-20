612452ab431a63aeabe6de8c36cb52fba281e9e1
sailendrachettri <sailendra9083@gmail.com>
2022-07-20 14:42:46 +0530

    conditionals in js

---
+++ JsHitesh/basic/conditionals__.js
@@ -0,0 +1,17 @@
+let temperature;
+temperature = 29;
+
+console.log();
+console.log();
+
+if(temperature <= 20){
+    console.log("Cold outside!");
+}
+
+if(temperature <= 30){
+    console.log("Moderate outside!");
+}
+
+
+console.log();
+console.log();
