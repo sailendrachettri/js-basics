aa18f9638ba5b2240572c70d06dfa0ac0658c625
sailendrachettri <sailendra9083@gmail.com>
2022-12-15 09:16:25 +0530

    objects in js

---
+++ jsHarryRivision/practice/04_practice.js
@@ -0,0 +1,20 @@
+let a = "Sailendra";
+let b = 5;
+
+// console.log(a + b);
+
+// console.log(typeof (a+b) );
+
+const bio = {
+    name: "Sailendra",
+    age : 21,
+    gender: "Male"
+}
+
+console.log(bio);
+// bio["name"] = "Rahul";
+// console.log(bio);
+
+console.log(bio.name);
+console.log(bio.age);
+console.log(bio.gender);
