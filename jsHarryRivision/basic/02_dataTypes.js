6564c8dbfb7eba5838aa259e77adbbbd97f611fa
sailendrachettri <sailendra9083@gmail.com>
2022-12-15 08:53:15 +0530

    primitive data types in js

---
+++ jsHarryRivision/basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+// 7 types of primitive data types
+/*
+N - null
+N - number
+B - boolean
+B - bigInt
+S - string
+S - symbol
+U - undefined
+*/
+
+
+let a = null;
+let b = 453;
+let c = true;
+let d = BigInt("4332344"); 
+let e = "String";
+let f = Symbol("I am a nice symbol");
+// let g = undefined;
+let g;
+console.log(typeof g);
+
+console.log("Code compiled successfully");
