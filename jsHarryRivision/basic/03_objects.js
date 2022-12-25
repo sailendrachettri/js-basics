51b309928f7d9a0129ae968e9e1c839d4a22b2fb
sailendrachettri <sailendra9083@gmail.com>
2022-12-25 08:38:50 +0530

    practice set 1 complected

---
+++ jsHarryRivision/basic/03_objects.js
@@ -0,0 +1,25 @@
+// object is a non-primitive datatypes
+
+const items = {
+    "Sai" : 10,
+    "Len" : 20,
+    "Dra" : 30,
+    "Che" : null,
+    "ttri" : undefined
+}
+
+// console.log(items);
+// console.log(items["Sai"]);
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("End");
