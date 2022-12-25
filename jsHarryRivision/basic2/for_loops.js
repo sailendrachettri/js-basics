6e203f44473f85fc8c4f3bc51ee01fd52badc888
sailendrachettri <sailendra9083@gmail.com>
2022-12-25 14:17:43 +0530

    for loops

---
+++ jsHarryRivision/basic2/for_loops.js
@@ -0,0 +1,30 @@
+// console.log("For loops");
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+console.log("i = " + i);
+
+/*
+
+// for in loop 
+let obj = {
+    sai : 10,
+    len : 20,
+    dra : 30
+}
+
+for(let key in obj)
+{
+    // console.log(key);
+    console.log("Marks of " + key + " are " + obj[key]);
+}
+
+// for of loop
+for(let alp of "Sailendra")
+{
+    console.log(alp);
+}
+
+*/
