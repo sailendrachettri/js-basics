db5fa902610e405ab9a5f583ec9669d8885fb762
sailendrachettri <sailendra9083@gmail.com>
2022-12-25 10:25:29 +0530

    conditionals

---
+++ jsHarryRivision/basic2/conditional_expression.js
@@ -0,0 +1,11 @@
+// let a = prompt("Enter you age dude: ");
+
+// let a = '10';
+// console.log(typeof a);
+
+// a = Number.parseInt(a);
+// console.log(typeof a);
+
+let age = 0;
+
+(age > 5) ? console.log("You are above 5") : console.log("You're below 5");
---
+++ jsHarryRivision/basic2/operators__.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 10;
+
+console.log(a == b); // true 
+console.log(a === b); // true 
+
+console.log(a != b); // false
+console.log(a !== b); // false 
