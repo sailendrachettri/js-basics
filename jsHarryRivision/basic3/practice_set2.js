508e06d2fca68da63cc0b5c49bec27e8a641c225
sailendrachettri <sailendra9083@gmail.com>
2022-12-27 21:17:09 +0530

    practic set 2

---
+++ jsHarryRivision/basic3/practice_set2.js
@@ -0,0 +1,11 @@
+let marks  = {
+    sailendra: 100,
+    rahul: 40,
+    uraj: 54,
+    udai: 89
+}
+
+for(let student in marks)
+{
+    console.log("Marks of " + student + " is " + marks[student]);
+}
