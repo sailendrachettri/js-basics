a6a06726561f8b7968372843736ab9e473861b2d
sailendrachettri <sailendra9083@gmail.com>
2022-12-28 10:08:23 +0530

    practice set strings

---
+++ jsHarryRivision/basic3/practice_set4.js
@@ -0,0 +1,6 @@
+let str = "Please give Rs 1000";
+let amount = console.log(str.slice("Please give Rs ".length) ); // it starts from that length and return from that index to till end
+
+// amount = Number.parseInt(amount);
+// amount += 13;
+// console.log(amount);
