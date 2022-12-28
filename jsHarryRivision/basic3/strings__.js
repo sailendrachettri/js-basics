74173ba9d188889f44cb149e9d4a946864231a8e
sailendrachettri <sailendra9083@gmail.com>
2022-12-28 09:47:47 +0530

    string methods

---
+++ jsHarryRivision/basic3/strings__.js
@@ -0,0 +1,12 @@
+let myName = "Saailendra";
+
+// string methods
+console.log(myName.length);
+console.log(myName.toUpperCase() );
+console.log(myName.toLowerCase() );
+console.log(myName.slice(3, 6) ); // first index included and last index is not included
+console.log(myName.replace('a', 'z') ); // only first will get replaced
+console.log(myName.trim() );
+
+
+console.log(myName.concat(" is my name") );
