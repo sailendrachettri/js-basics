442b42152983650f4fae377de83b50dfb357803b
sailendrachettri <sailendra9083@gmail.com>
2022-12-28 10:20:31 +0530

    arrays in js

---
+++ jsHarryRivision/intermediate1/arrays__.js
@@ -0,0 +1,12 @@
+// arrays in javascript
+
+let marks12 = [89, 54, 29, 90, 32, null, false, undefined];
+console.log(marks12);
+// console.log(typeof marks12);
+
+console.log(marks12.length);
+marks12[30] = null;
+console.log(marks12.length);
+
+
+console.log(marks12[28]);
