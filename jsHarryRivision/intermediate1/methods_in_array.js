7a72e26cabd4f4a7920bdee6699551786e0777e5
sailendrachettri <sailendra9083@gmail.com>
2022-12-28 11:03:49 +0530

    array common methods

---
+++ jsHarryRivision/intermediate1/methods_in_array.js
@@ -0,0 +1,27 @@
+let naturalNum = [1, 2, 3, 4];
+
+
+// let naturalNumString = naturalNum.toString();
+// console.log(naturalNumString);
+// console.log(typeof naturalNumString);
+
+
+// let joinUndescore = naturalNum.join('_');
+// console.log(joinUndescore);
+// console.log(typeof joinUndescore);
+
+// console.log(naturalNum);
+// naturalNum.pop();
+// console.log(naturalNum);
+
+// console.log(naturalNum);
+// naturalNum.push(5);
+// console.log(naturalNum);
+
+// console.log(naturalNum);
+// naturalNum.shift(); // removes and return from beginning of an array
+// console.log(naturalNum);
+
+// console.log(naturalNum);
+// naturalNum.unshift(5); // removes and replace from start
+// console.log(naturalNum);
