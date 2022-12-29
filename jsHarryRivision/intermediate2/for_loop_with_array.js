5c5128e8aa27467f8d88da276527f7539eab5db5
sailendrachettri <sailendra9083@gmail.com>
2022-12-29 09:54:48 +0530

    loops in array

---
+++ jsHarryRivision/intermediate2/for_loop_with_array.js
@@ -0,0 +1,32 @@
+let nums = [2, 3, 4, 5, 7];
+
+
+// normal for loop
+// for(let i = 0; i < nums.length; i++)
+// {
+//     console.log(nums[i]);
+// }
+
+
+// for each loop
+// nums.forEach( (element) => {
+//     console.log(element*element);
+// })
+
+// Array.from
+// let myName = "Sailendra";
+// let myNameArr = Array.from(myName);
+// console.log(myNameArr);
+
+// for...of loop
+// for(let i of nums)
+// {
+//     console.log(i); // gives element
+// }
+
+// for...in loop
+// for(let i in nums)
+// {
+//     // console.log(i); // it gives keys
+//     console.log(nums[i]); // it gives values
+// }
