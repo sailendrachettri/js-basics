a1d6ccc1c10275d00ae816fc2c786d0a4ec455a9
sailendrachettri <sailendra9083@gmail.com>
2022-12-30 10:21:56 +0530

    console objects

---
+++ jsHarryRivision/intermediate2/consolce_obj.js
@@ -0,0 +1,18 @@
+// console.log(console)
+// console.warn("warning");
+// console.assert(4 < 5);
+
+// console.clear();
+
+let obj = {
+    sailendra: 10,
+    rahul: 30,
+    udai: 90,
+    neel: 87,
+    top: 43
+}
+
+console.time("console");
+console.table(obj);
+console.timeEnd("console");
+
