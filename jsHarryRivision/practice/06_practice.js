fa2a9cd00ec1b1313dc0168ba74728b7632bbd88
sailendrachettri <sailendra9083@gmail.com>
2022-12-31 08:32:16 +0530

    practice set 6

---
+++ jsHarryRivision/practice/06_practice.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter a number: ");
+num = Number.parseInt(num);
+(num > 4) ? (location.href = "https://laughdude.netlify.app") : console.log("Your number is " + num);
+
+
+// let userColorBg = prompt("Enter your favourite color");
+// document.body.style.background = userColorBg;
