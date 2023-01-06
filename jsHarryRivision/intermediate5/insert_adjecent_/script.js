6898b99093451d4cd400a2f403c9fbc983f7841d
sailendrachettri <sailendra9083@gmail.com>
2023-01-06 12:39:48 +0530

    insert adjecent elements

---
+++ jsHarryRivision/intermediate5/insert_adjecent_/script.js
@@ -0,0 +1,8 @@
+let firstDiv = document.getElementById("firstDiv");
+
+// firstDiv.insertAdjacentHTML('beforebegin', "<div>beforebegin</div");
+// firstDiv.insertAdjacentHTML('beforeend', "<div>beforeend</div");
+// firstDiv.insertAdjacentHTML('afterbegin', "<div>afterbegin</div");
+// firstDiv.insertAdjacentHTML('afterend', "<div>afterend</div");
+
+firstDiv.remove();
