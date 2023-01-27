66df76607e081753b6197a07ad097608a6af5c47
sailendrachettri <sailendra9083@gmail.com>
2023-01-27 09:40:41 +0530

    changing class names

---
+++ jsHarryRivision/intermediate5/changingClasses/script.js
@@ -0,0 +1,5 @@
+// firstDiv.className = "red"; // change class name dynamically
+
+// firstDiv.classList.remove('yellow') // remove specific class 
+// firstDiv.classList.remove('red') // add specific class 
+// firstDiv.classList.toggle('red') // toggle specific class 
